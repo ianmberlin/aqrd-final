0a4d3100248b5cad78e13abcf9f7c20e22d4d4ae
Ian Berlin <ian.berlin@yale.edu>
2023-12-12 16:33:33 -0500

    Started fuzzy matching, cleaned out extra code at bottom

--- Forbes.R
+++ Forbes.R
@@ -1,7 +1,6 @@
 library(tidyverse) 
 library(haven)
 library(modelsummary)
-library(summarytools)
 library(vtable)
 library(fixest)
 library(gt)
@@ -13,31 +12,44 @@ forbes <- read_dta("data/brookings_forbes_2022.dta")
 state_year<- read_dta("ReplicationPackage/data/stata_data/StateyearAnalysisDataset.dta")
 state_year_age <- read_dta("ReplicationPackage/data/stata_data/StateyearAgeAnalysisDataset.dta")
 person_year <- read_dta("ReplicationPackage/data/stata_data/IndivAnalysisDataset.dta") |>
-  rename(name = Name) 
+  rename(name = Name) |>
+  filter(year != "1982")
 
-dynastic <- read_excel("data/Forbes 400 Aggregate with Dynasty Members.xlsx")
+dynastic <- read_excel("data/Forbes 400 Aggregate with Dynasty Members, 2017 end.xlsx")
 
-dynasty_test <- dynastic |> 
-  slice(1:330)
-
-dynasty_long <- dynasty_test |>
-  filter(dynasty_binary != "") |> 
+dynasty_clean <- dynastic |>
+  filter(dynasty_binary != "") |>
   mutate(dynasty_binary = as.character(dynasty_binary)) |> 
   select(!"Grand Total") |>
+  mutate(name = gsub("(Child|Spouse).*", "", name))
+
+moretti_names <- person_year |>
+  select(name) |>
+  unique()
+
+
+dynasty_long <- dynastic |>
+  filter(dynasty_binary != "") |> 
+  mutate(dynasty_binary = as.character(dynasty_binary)) |> 
+  select(!"Grand Total") |> 
   pivot_longer(cols = !c("name", "dynasty_binary"),
                names_to = "year",
                values_to = "NetWorth") |>
-  filter(NetWorth != "") |>
-  mutate(NetWorthMill = NetWorth/1000000)
+  filter(NetWorth != "",
+         !year %in% c("2018", "2019", "2020")) |>
+  mutate(NetWorthMill = NetWorth/1000000) 
 
-dynasty_long |>
-  fuzzy_inner_join(
-    person_year,
-    by = c("year", "name", "NetWorthMill"),
-    match_fun = stringdist_inner_join,
-    distance_col = "distance"
-  )
 
+
+join <- stringdist_inner_join(dynasty_clean, moretti_names, 
+                by='name',
+                max_dist = 3) |>
+  select(c("name.x", "name.y", "dynasty_binary"))
+  
+  
+  #match based on tea) |>
+
+?stringdist_inner_join
 #test for git 
 
 
@@ -52,13 +64,20 @@ state_year <- state_year |>
   mutate(pop_90to97_natl = sum(pop_90to97)) |>
   mutate(topshr_90to97 = (pop_90to97/pop_90to97_natl)*100) |>
   mutate(total_wealth = sum(wealth)) |>
-  mutate(wealth_share = (wealth/total_wealth)*100)
+  mutate(wealth_share = (wealth/total_wealth)*100) |>
+  mutate(stockpc = (stock/pop)*1000) |>
+  mutate(inheritance_estate = if_else(EI == 1 | Ionly == 1, 1, 0))
+
+state_year$Ionly
 
 mod1 <- feols(stock ~ EI*post_year + EI  | State + year, state_year)
 mod2 <- feols(stock ~ EI*post_year + EI + PIT*post_year + PIT | State + year, state_year)
 mod3 <- feols(stock ~ EI*post_year + EI + topshr_90to97  | State + year, state_year)
-mod6 <- feols(wealth_share ~ EI*post_year + EI  | State + year, state_year)
+mod5 <- feols(stockpc ~ EI*post_year + EI  | State + year, state_year)
+mod6 <- feols(wealth ~ EI*post_year + EI  | State + year, state_year)
+mod7 <- feols(stock ~ inheritance_estate*post_year + inheritance_estate | State + year, state_year)
 
+summary(mod7)
 #create dataset without 2002-2004
 state_year_drop <- state_year|>
   filter(year != "2002",
@@ -68,13 +87,7 @@ state_year_drop <- state_year|>
 mod8 <- feols(stock ~ EI*post_year + EI  | State + year, state_year_drop)
 
 
-modelsummary(list(mod1, mod2, mod3, mod6, mod8), gof_map = c("nobs", "FE: State", "FE: year"))
-
-forbes |>
-  group_by(year) |>
-  count(marital) |>
-  ggplot(aes(x = year, y = n, color = marital)) +
-  geom_line()
+modelsummary(list(mod1, mod2, mod3, mod5, mod6, mod7, mod8), gof_map = c("nobs", "FE: State", "FE: year"))
 
 
 # Summary Statistics ----
@@ -91,73 +104,3 @@ summary_table <- forbes |>
            digits = 4)
 
 
-# Everything else ----
-  
-  summarize_all(list(mean = mean, sd = sd), na.rm = TRUE)
-
-pivot_wider(forbes, names_from = fullname, values_from = networth)
-
-
-ivys <- c("Harvard", "Princeton", "Yale", "Dartmouth", "Brown", "Columbia", "Cornell")
-
-forbes <- forbes |>
-  filter(year >= 2001) |>
-  mutate(ivy = ifelse(any(str_detect(education, ivys)), 1, 0)) |>
-  count(ivy)
-
-forbes |>
-  mutate(selfmade = tolower(selfmade),
-         selfmade = ifelse("self-made", 1, 0)) |>
-  group_by(year) |>
-  count(selfmade) |>
-  ggplot(aes(x = year, y = n, color = selfmade)) +
-  geom_point()
-
-filtered <- forbes |>
-  filter(year >= 2014) |>
-  mutate(self_score = SelfMadeScoreBeginning2014)
-
-mod <- lm(networth ~ state_current, data = filtered)
-
-summary(mod)
-
-states <- forbes |>
-  group_by(state, year) |>
-  count(state)
-
-forbes |>
-  ggplot(aes(x = year, y = sum, color = self)) +
-  geom_line()
-
-wyoming <- forbes |>
-  filter(year == 2009)
-
-forbes |>
-  group_by(year) |>
-  summarize(ratio = max(networth)/min(networth)) |>
-  ggplot(aes(x = year, y = ratio)) +
-  geom_line()
-
-test <- forbes[forbes$lastname == "Gates",]
-
-forbes <- forbes |>
-  mutate(selfmade2 = if_else(selfmade == "self-made", 1, 0))
-
-mod <- lm(networth ~ race , data = forbes)
-summary(mod)
-
-model1 <- feols(Change ~ networth | URL_name + year, forbes)
-
-summary(model1)
-
-forbes |>
-  group_by(year, race) |>
-  summarize(mean = mean(networth)) |>
-  pivot_wider(names_from = "race", values_from = "mean") |>
-  mutate(ratio = white/black) |>
-  ggplot(aes(x = year, y = ratio)) +
-  geom_line()
-  
-
-gt() |>
-  cols_label(c("year", "asian", "black", "hispanic", "white"))
